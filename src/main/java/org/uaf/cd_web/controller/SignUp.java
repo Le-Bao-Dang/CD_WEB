8b4d7c229463196b1828ff16019066de07deac15
Nguyễn Thị Xuân Hoa <113543191+Nguyen-Thi-Xuan-Hoa@users.noreply.github.com>
2024-04-27 14:57:29 +0700

    commmit

--- src/main/java/org/uaf/cd_web/controller/SignUp.java
+++ src/main/java/org/uaf/cd_web/controller/SignUp.java
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.uaf.cd_web.component.Encryption;
-import org.uaf.cd_web.entity.User;
 import org.uaf.cd_web.services.UserServiceImp;
+
 @Controller
 public class SignUp {
     @Autowired
@@ -28,15 +28,16 @@ public class SignUp {
                         RedirectAttributes redirectAttributes) {
         boolean exist = userService.checkUserExit(email, phone);
         if (name == null) {
-            redirectAttributes.addAttribute("errorName", "*Xin hãy nhập tên của bạn");
+            redirectAttributes.addFlashAttribute("errorName", "*Xin hãy nhập tên của bạn");
             return "redirect:/signup";
         }
         if (exist) {
-            redirectAttributes.addAttribute("errorAcc", "* Tài khoản đã được sử dụng");
+            redirectAttributes.addFlashAttribute("errorAcc", "* Tài khoản đã được sử dụng");
+            System.out.println();
             return "redirect:/signup";
 
         } else if (!passw.equals(repassw)) {
-            redirectAttributes.addAttribute("errorDup", "* Mật khẩu không trùng khớp");
+            redirectAttributes.addFlashAttribute("errorDup", "* Mật khẩu không trùng khớp");
             return "redirect:/signup";
         } else {
             String password = Encryption.toSHA1(passw);
